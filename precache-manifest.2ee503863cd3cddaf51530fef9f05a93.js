978a18e33bf1b207b9c26011e1823505487d0bbb
Pavel Ivchenko <pavel.ivchenko.js@mail.ru>
2020-07-20 22:12:35 +0300

    Updates

---
+++ precache-manifest.2ee503863cd3cddaf51530fef9f05a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c799c2c7f6a24b585af1180d67ddc173",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "1a5c24f0ace152a82463",
+    "url": "/notes-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a5c24f0ace152a82463",
+    "url": "/notes-app/static/js/2.ec1f3f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes-app/static/js/2.ec1f3f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2cb9e93388ecbbca87",
+    "url": "/notes-app/static/js/main.a99ec457.chunk.js"
+  },
+  {
+    "revision": "673dbc1f14a848599a5b",
+    "url": "/notes-app/static/js/runtime-main.bf95cf4d.js"
+  }
+]);
